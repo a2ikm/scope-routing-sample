6b642c8003acb60265bf786f7e7111faa83620a1
Masato Ikeda <masato.ikeda@gmail.com>
2011-07-11 22:59:14 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScopeRoutingSample::Application.config.secret_token = 'd47c45a50f2fcf5dd3cd0f8f44205ff298a9ece5359248fe3817718dc8a8cfaaaf6717b3416ecccb03727d562294f2116b9e90e598b1a1e02cfa42220ad9e477'
